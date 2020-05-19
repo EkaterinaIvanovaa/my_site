afd022fa6c526126139479230472002c27e96b08
EkaterinaIvanovaa <ekaterinaivanova3190@gmail.com>
2020-05-20 03:54:23 +0500

    Delete 2 line

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z_pp*2@2r4&&4)zttpc1ys%))-a2_*&@*pyhl9^ee(vpeb$q@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -120,11 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-import dj_database_url
-db_from_evn = dj_database_url.config()
-DATABASES['default'].update(db_from_evn)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
